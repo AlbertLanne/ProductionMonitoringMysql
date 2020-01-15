9d31073d658b134c34379e1d8271b37e69b6028e
AlbertLanne <albert.lanne@gmail.com>
2020-01-15 21:24:37 +0100

    add statisitque page

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 // viewed at http://localhost:8080
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/statistique/statistique.html'));
 });
 
 app.listen(8080);
