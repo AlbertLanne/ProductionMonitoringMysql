26bdaeaf4ae206a216a6df517fb0f80ec103173b
AlbertLanne <albert.lanne@gmail.com>
2020-01-16 17:36:21 +0100

    add api

---
+++ server/start.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+// API SERVER HTTP AXIOS HERE
+const app = express();
+
+// MIDDLEWARE
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const informations = require('./informations');
+
+
+// Le lien avec le serveur d'api   ==>  http://localhost:4000/api/informations
+app.use('/api/informations', informations);
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log(`L'api fonctionne sur le port ${port}`));
