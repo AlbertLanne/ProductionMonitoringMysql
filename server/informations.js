044dc13dc0a39756c0d9f256b8af9edd86680858
AlbertLanne <albert.lanne@gmail.com>
2020-01-17 11:23:10 +0100

    api work

--- server/informations.js
+++ server/informations.js
@@ -2,30 +2,36 @@ const express = require('express');
 var mysql = require('mysql');
 const router = express.Router();
 
-
+var renvoi = [];
 router.get('/', async (req, res) => {
-  const informations = await loadProductsCollection();
-  res.send(informations);
-  // res.send(await informations.find([]).toArray());
-  console.log(informations);
-  console.log("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
-  // console.log(await informations.find({}).toArray());
+
+// c'est informations qui prend l'array vous pouvez faire un test avec un array statique
+//     var cars = [
+//         "Saab",
+//         "Volvo",
+//         "BMW",
+//     ];
+    const informations = await loadProductsCollection();
+    res.send(await informations);
+    console.log(informations);
 });
 
 async function loadProductsCollection() {
-  var con = mysql.createConnection('mysql://uisomclwcgug5cj5:58Eg8vzqeQ4Rx0zxjhFw@bfgvnm6ajhbocjxbjmly-mysql.services.clever-cloud.com:3306/bfgvnm6ajhbocjxbjmly');
-  connection = con.connect(function(err) {
-    if (err) throw err;
-    //Select all customers and return the result object:
 
-    con.query("SELECT * FROM `producttable`", function (err, result, fields) {
-      if (err) throw err;
-      // Retourne toute la table producttable et sont contenu dans la var result
-      // console.log(result);
-      console.log(result.find({}).toArray());
+    var con = mysql.createConnection('mysql://uisomclwcgug5cj5:58Eg8vzqeQ4Rx0zxjhFw@bfgvnm6ajhbocjxbjmly-mysql.services.clever-cloud.com:3306/bfgvnm6ajhbocjxbjmly');
+    connection = con.connect(function (err) {
+        if (err) throw err;
+        //Select all customers and return the result object:
 
+        con.query("SELECT * FROM `producttable`", function (err, result, fields) {
+            if (err) throw err;
+            // Retourne toute la table producttable et sont contenu dans la var result
+            // console.log(result);
+            renvoi = result;
+            // console.log(result.find({}).toArray());
+        });
     });
-  });
+    return renvoi;
 }
 
 
@@ -50,11 +56,6 @@ async function loadProductsCollection() {
 
 
 module.exports = router;
-
-
-
-
-
 
 
 //
