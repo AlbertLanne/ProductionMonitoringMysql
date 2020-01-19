e29ecccf5c1075aa129b596f2924cfadb0f3a9cf
AlbertLanne <albert.lanne@gmail.com>
2020-01-19 17:07:05 +0100

    review api

--- informations.js
+++ informations.js
@@ -1,27 +1,32 @@
 import axios from "axios";
 
+
 const url = "http://localhost:4000/api/informations";
 
 class informationsinformations {
     // initialise the data at the url string
-
     static getinformations() {
-        return new Promise(async (resolve, reject) => {
-            try {
-                const res = await axios.get(url);
-                const data = res.data;
-                resolve(
-                    data.map(post => ({
-                        ...post,
-                        createdAt: new Date(post.createdAt)
-                    }))
-                );
-            }
-            catch (err) {
-                reject(err);
-            }
-        });
+                    const res = axios.get(url);
+                    const data = res.data;
+                    return data;
     }
+    // static getinformations() {
+    //     return new Promise(async (resolve, reject) => {
+    //         try {
+    //             const res = await axios.get(url);
+    //             const data = res.data;
+    //             resolve(
+    //                 data.map(post => ({
+    //                     ...post,
+    //                     createdAt: new Date(post.createdAt)
+    //                 }))
+    //             );
+    //         }
+    //         catch (err) {
+    //             reject(err);
+    //         }
+    //     });
+    // }
 
     // static methode create
 
--- server/informations.js
+++ server/informations.js
@@ -6,29 +6,25 @@ var renvoi = [];
 router.get('/', async (req, res) => {
 
 // c'est informations qui prend l'array vous pouvez faire un test avec un array statique
-//     var cars = [
-//         "Saab",
-//         "Volvo",
-//         "BMW",
-//     ];
-    const informations = await loadProductsCollection();
+
+ cars = [{"salesid":1,"quantity":11,"date":"2020-01-31T23:00:00.000Z"},{"salesid":2,"quantity":3,"date":"2018-12-31T23:00:00.000Z"},{"salesid":7,"quantity":9,"date":"2018-08-31T22:00:00.000Z"}]
+    informations = cars;
+    // const informations = await loadProductsCollection();
     res.send(await informations);
-    // console.log(informations);
 });
 
-async function loadProductsCollection() {
+    async function loadProductsCollection() {
 
     var con = mysql.createConnection('mysql://uisomclwcgug5cj5:58Eg8vzqeQ4Rx0zxjhFw@bfgvnm6ajhbocjxbjmly-mysql.services.clever-cloud.com:3306/bfgvnm6ajhbocjxbjmly');
     connection = con.connect(function (err) {
         if (err) throw err;
         //Select all customers and return the result object:
-
         con.query("SELECT * FROM `producttable`", function (err, result, fields) {
             if (err) throw err;
            renvoi = result;
+            });
         });
-    });
-    return renvoi;
-}
+        return renvoi;
+    }
 
 module.exports = router;
