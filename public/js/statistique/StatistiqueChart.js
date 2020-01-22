1bf93c46131c068fb0ed1bec3270c9b0001df040
AlbertLanne <albert.lanne@gmail.com>
2020-01-22 14:57:33 +0100

    up

--- public/js/statistique/StatistiqueChart.js
+++ public/js/statistique/StatistiqueChart.js
@@ -1,37 +1,82 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var ctx=document.getElementById('myChart');
+
+let test;
+
+function getRandomInt() {
+    return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+}
+ function putdata() {
+    let response =  fetch(`http://localhost:4000/api/informations`);
+    // let data = await response.json();
+    // resultElement.innerHTML = JSON.stringify(data, null, '\t') ;
+    // let truc = JSON.stringify(data, null, '\t');
+
+     let clone = JSON.parse(JSON.stringify(this.response));
+     clone[0].data = data;
+     this.barChartData = clone;
+     console.log(clone);
+
+
+ }
+
+
+async function performGetRequest() {
+    var resultElement = await document.getElementById('getResult1');
+    resultElement.innerHTML = '';
+
+    let response = await fetch(`http://localhost:4000/api/informations`);
+    let data = await response.json()
+
+    resultElement.innerHTML = '<div class="bg-red-300 flex">' + '<p class="">' + "Status -> " + '<p>' +
+        response.status + ' ' + response.statusText + '</div>' +
+        '<p class="text-xl">Content:</p>' + '<p class="">' + '<p>' +
+        '<pre>' + JSON.stringify(data, null, '\t') + '</pre>';
+}
+
+async function performGetRequest1() {
+    var resultElement = await document.getElementById('getResult2');
+    resultElement.innerHTML = '';
+
+    let response = await fetch(`http://localhost:4000/api/informations`);
+    let data = await response.json();
+    test = Object.values(data[1]);
+    let digit = Object.values(data[1])[0];
+    resultElement.innerHTML = test + 'Le n°0 de lalgo est ' + digit;
+    // resultElement.innerHTML = JSON.stringify(data[1], null, '\t') + ' '+
+    //     JSON.stringify(test, null, '\t');
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var ctx = document.getElementById('myChart');
 
     var graphique = new Chart(ctx.getContext('2d'), {
-        type: 'bar',
+        type: 'line',
         data: {
-            labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet'],
+            labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai'],
             datasets: [{
-                label: 'Stock des grillages',
-                backgroundColor: '#f87979',
-                borderColor: 'rgb(242,255,250)',
-                data: [getRandomInt(), getRandomInt(), getRandomInt(), getRandomInt(), getRandomInt(), getRandomInt(), getRandomInt()]
-            }]
+                label: 'Data useless',
+                backgroundColor: "rgba(192,0,15,0.4)",
+                borderColor: "rgb(192,140,148)",
+                borderCapStyle: 'butt',
+                fill: false,
+                data: [getRandomInt(), getRandomInt(), getRandomInt(), getRandomInt(), getRandomInt()]
+            }],
         },
     })
 
-    function getRandomInt() {
-        return Math.floor(Math.random() * (50 - 5 + 1)) + 5
 
-    }
+    document.getElementById("SelectBox").addEventListener("change", swapGraph);
+    document.getElementById("SelectBox").addEventListener("change", onHide);
 
-    document.getElementById("SelectBox").addEventListener("change",swapGraph);
-    document.getElementById("SelectBox").addEventListener("change",onHide)        ;
-
-    function onHide(){
-        if (document.getElementById("SelectBox").value == "default"){
+    function onHide() {
+        if (document.getElementById("SelectBox").value == "default") {
             document.getElementById("myChart").style.visibility = 'hidden';
-        }
-        else document.getElementById("myChart").style.visibility ='visible';
+        } else document.getElementById("myChart").style.visibility = 'visible';
 
     }
 
-    function swapGraph(){
-        if(document.getElementById("SelectBox").value == "blindax"){
+    function swapGraph() {
+        if (document.getElementById("SelectBox").value == "blindax") {
             graphique.data.datasets[0].data[0] = [60];
             graphique.data.datasets[0].data[1] = [60];
             graphique.data.datasets[0].data[2] = [60];
@@ -40,9 +85,7 @@ document.addEventListener('DOMContentLoaded', function() {
             graphique.data.datasets[0].data[5] = [60];
             graphique.data.datasets[0].data[6] = [60];
 
-        }
-        else
-        {
+        } else {
             graphique.data.datasets[0].data[0] = [40];
             graphique.data.datasets[0].data[1] = [40];
             graphique.data.datasets[0].data[2] = [60];
@@ -53,44 +96,14 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
 
-
         // graphique.config.data.labels =["Test","Charlie"];
         graphique.update();
 
     }
 
 
-
     onHide();
 });
-
-
-
-
-
-
-function getChartStatistique() {
-        var elChart = document.getElementById("myChart1");
-        if (elChart != undefined) {
-
-            new Chart(elChart.getContext('2d'), {
-                type: 'bar',
-                data: {
-                    labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet'],
-                    datasets: [{
-                        label: 'Stock des grillages',
-                        backgroundColor: '#f87979',
-                        borderColor: 'rgb(242,255,250)',
-                        data: [getRandomInt(), getRandomInt(), getRandomInt(), getRandomInt(), getRandomInt(), getRandomInt(), getRandomInt()]
-                    }]
-                },
-            })
-
-        }
-    }
-
-    getChartStatistique();
-
 
 
 
