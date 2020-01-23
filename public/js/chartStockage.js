1515673d4d1db3d5c21832d806844e9e836662e6
Aredi <dadaslashfaq@gmail.com>
2020-01-23 22:27:35 +0100

    fix json usage

--- public/js/chartStockage.js
+++ public/js/chartStockage.js
@@ -1,114 +1,119 @@
 
 document.addEventListener('DOMContentLoaded', function() {
 
+    var labels = [];
+    var donnee = [];
+
     var ctx=document.getElementById('myChart');
 
-var charlie = [];
-    var testlol = [];
 
-    function ajaxGet(url, callback) {
-        var req = new XMLHttpRequest();
-        req.open("GET", url);
-        req.addEventListener("load", function () {
-            if (req.status >= 200 && req.status < 400) {
-                // Appelle la fonction callback en lui passant la réponse de la requête
-                callback(req.responseText);
-            } else {
-                console.error(req.status + " " + req.statusText + " " + url);
-            }
+    async function performGetRequest() {
+        let response = await fetch(`http://localhost:4000/api/informations`);
+        let data = await response.json();
+        let labels = data.map(function(e){
+            return e.id;
         });
-        req.addEventListener("error", function () {
-            console.error("Erreur réseau avec l'URL " + url);
+        let donnee = data.map(function(e){
+            return e.first_name;
         });
-        req.send(null);
-    }
-
-    ajaxGet("http://localhost:4000/api/informations",function(reponse){
-         charlie =  console.log(reponse);
-        console.log(JSON.parse(charlie));
-    });
-
-testlol.jsonarray.map(function(e){
-    console.log(e.name);
-});
-
-
-
-getFullName(charlie);
-
-
-        var graphique = new Chart(ctx.getContext('2d'), {
-            type: 'bar',
-            data: {
-                labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet'],
-                datasets: [{
-                    label: 'age',//'Stock des grillages en metal',
-                    backgroundColor: [
-                        'rgba(255,99,132,0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: 'rgb(242,255,250)',
-                    data: [getRandomInt(), getRandomInt(), getRandomInt(), getRandomInt(), getRandomInt(), getRandomInt(), getRandomInt()]
-                }]
-            },
-        })
-
-
-
-
-function getRandomInt() {
-    return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
-
-}
-
-    document.getElementById("SelectBox").addEventListener("change",swapGraph);
-    document.getElementById("SelectBox").addEventListener("change",onHide)        ;
-
-    function onHide(){
-        if (document.getElementById("SelectBox").value == "default"){
-            document.getElementById("myChart").style.visibility = 'hidden';
-        }
-         else document.getElementById("myChart").style.visibility ='visible';
-
-    }
-
- function swapGraph(){
-                if(document.getElementById("SelectBox").value == "blindax"){
-                    graphique.data.datasets[0].data[0] = [10];
-                    graphique.data.datasets[0].data[1] = [60];
-                    graphique.data.datasets[0].data[2] = [60];
-                    graphique.data.datasets[0].data[3] = [60];
-                    graphique.data.datasets[0].data[4] = [60];
-                    graphique.data.datasets[0].data[5] = [60];
-                    graphique.data.datasets[0].data[6] = [60];
-
-                }
-                else
-                {
-                    graphique.data.datasets[0].data[0] = [40];
-                    graphique.data.datasets[0].data[1] = [40];
-                    graphique.data.datasets[0].data[2] = [60];
-                    graphique.data.datasets[0].data[3] = [60];
-                    graphique.data.datasets[0].data[4] = [60];
-                    graphique.data.datasets[0].data[5] = [60];
-                    graphique.data.datasets[0].data[6] = [60];
-                }
-
-
-
-                // graphique.config.data.labels =["Test","Charlie"];
-                graphique.update();
-
+        console.log(donnee);
+        console.log(labels);
     }
 
 
 
 
-     onHide();
+
+
+
+    // function ajaxGet(url, callback) {
+    //     var req = new XMLHttpRequest();
+    //     req.open("GET", url);
+    //     req.addEventListener("load", function () {
+    //         if (req.status >= 200 && req.status < 400) {
+    //             // Appelle la fonction callback en lui passant la réponse de la requête
+    //             callback(req.responseText);
+    //         } else {
+    //             console.error(req.status + " " + req.statusText + " " + url);
+    //         }
+    //     });
+    //     req.addEventListener("error", function () {
+    //         console.error("Erreur réseau avec l'URL " + url);
+    //     });
+    //     req.send(null);
+    // }
+    //
+    // ajaxGet("http://localhost:4000/api/informations",function(reponse){
+    //      charlie =  console.log(reponse);
+    //     console.log(JSON.parse(charlie));
+    // });
+
+
+
+
+    var graphique = new Chart(ctx.getContext('2d'), {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                data: donnee
+            }],
+        },
+    })
+
+
+
+
+// function getRandomInt() {
+//     return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
+//
+// }
+//
+//     document.getElementById("SelectBox").addEventListener("change",swapGraph);
+//     document.getElementById("SelectBox").addEventListener("change",onHide)        ;
+//
+//     function onHide(){
+//         if (document.getElementById("SelectBox").value == "default"){
+//             document.getElementById("myChart").style.visibility = 'hidden';
+//         }
+//          else document.getElementById("myChart").style.visibility ='visible';
+//
+//     }
+//
+//  function swapGraph(){
+//                 if(document.getElementById("SelectBox").value == "blindax"){
+//                     graphique.data.datasets[0].data[0] = [10];
+//                     graphique.data.datasets[0].data[1] = [60];
+//                     graphique.data.datasets[0].data[2] = [60];
+//                     graphique.data.datasets[0].data[3] = [60];
+//                     graphique.data.datasets[0].data[4] = [60];
+//                     graphique.data.datasets[0].data[5] = [60];
+//                     graphique.data.datasets[0].data[6] = [60];
+//
+//                 }
+//                 else
+//                 {
+//                     graphique.data.datasets[0].data[0] = [40];
+//                     graphique.data.datasets[0].data[1] = [40];
+//                     graphique.data.datasets[0].data[2] = [60];
+//                     graphique.data.datasets[0].data[3] = [60];
+//                     graphique.data.datasets[0].data[4] = [60];
+//                     graphique.data.datasets[0].data[5] = [60];
+//                     graphique.data.datasets[0].data[6] = [60];
+//                 }
+//
+//
+//
+//                 // graphique.config.data.labels =["Test","Charlie"];
+//                 graphique.update();
+//
+//     }
+
+
+
+
+     // onHide();
+    performGetRequest();
 });
 
 
--- server/api.js
+++ server/api.js
@@ -5,29 +5,25 @@ const router = express.Router();
 var renvoi = [];
 router.get('/', async (req, res) => {
 
- // informations  prend l'array vous pouvez faire un test avec un array statique comme ci dessous en commentaire
-    informations = [{"salesid":1,"quantity":11,"date":"2020-01-31T23:00:00.000Z"},{"salesid":2,"quantity":3,"date":"2018-12-31T23:00:00.000Z"},{"salesid":3,"quantity":3,"date":"2018-08-31T22:00:00.000Z"},{"salesid":4,"quantity":9,"date":"2018-08-31T22:00:00.000Z"},{"salesid":5,"quantity":9,"date":"2018-08-31T22:00:00.000Z"},{"salesid":6,"quantity":9,"date":"2018-08-31T22:00:00.000Z"},{"salesid":7,"quantity":9,"date":"2018-08-31T22:00:00.000Z"},{"salesid":8,"quantity":9,"date":"2018-08-31T22:00:00.000Z"},{"salesid":9,"quantity":9,"date":"2018-08-31T22:00:00.000Z"},{"salesid":10,"quantity":9,"date":"2018-08-31T22:00:00.000Z"},{"salesid":11,"quantity":9,"date":"2018-08-31T22:00:00.000Z"},{"salesid":12,"quantity":9,"date":"2018-08-31T22:00:00.000Z"}]
+    // informations  prend l'array vous pouvez faire un test avec un array statique comme ci dessous en commentaire
+
     // const informations = await loadProductsCollection();
-   // informations = [
-   //     {firstname : "Malcom", lastname: "Reynolds"},
-   //     {firstname : "Kaylee", lastname: "Frye"},
-   //     {firstname : "Jayne", lastname: "Cobb"}
-   // ];
+    informations = [{"salesid":1,"quantity":11,"date":"2020-01-31T23:00:00.000Z"},{"salesid":2,"quantity":3,"date":"2018-12-31T23:00:00.000Z"},{"salesid":7,"quantity":9,"date":"2018-08-31T22:00:00.000Z"},{"salesid":7,"quantity":9,"date":"2018-08-31T22:00:00.000Z"},{"salesid":7,"quantity":9,"date":"2018-08-31T22:00:00.000Z"},{"salesid":7,"quantity":9,"date":"2018-08-31T22:00:00.000Z"},{"salesid":7,"quantity":9,"date":"2018-08-31T22:00:00.000Z"},{"salesid":7,"quantity":9,"date":"2018-08-31T22:00:00.000Z"},{"salesid":7,"quantity":9,"date":"2018-08-31T22:00:00.000Z"},{"salesid":7,"quantity":9,"date":"2018-08-31T22:00:00.000Z"},{"salesid":7,"quantity":9,"date":"2018-08-31T22:00:00.000Z"},{"salesid":7,"quantity":9,"date":"2018-08-31T22:00:00.000Z"}]
     // const informations = await loadProductsCollection();
     res.send(await informations);
 });
 
-    async function loadProductsCollection() {
+async function loadProductsCollection() {
     var con = mysql.createConnection('mysql://uisomclwcgug5cj5:58Eg8vzqeQ4Rx0zxjhFw@bfgvnm6ajhbocjxbjmly-mysql.services.clever-cloud.com:3306/bfgvnm6ajhbocjxbjmly');
     connection = con.connect(function (err) {
         if (err) throw err;
         //Select all customers and return the result object:
         con.query("SELECT * FROM `producttable`", function (err, result, fields) {
             if (err) throw err;
-           renvoi = result;
-            });
+            renvoi = result;
         });
-        return renvoi;
-    }
+    });
+    return renvoi;
+}
 
 module.exports = router;
