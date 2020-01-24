dc00125190926ba8ddcb0ac55248f618af296922
Aredi <dadaslashfaq@gmail.com>
2020-01-24 09:31:43 +0100

    fix

--- server/api.js
+++ server/api.js
@@ -7,36 +7,37 @@ router.get('/', async (req, res) => {
 
     // informations  prend l'array vous pouvez faire un test avec un array statique comme ci dessous en commentaire
 
-    // const informations = await loadProductsCollection();
-    informations = [{
-        "id": 40,
-        "first_name": "Jeanette",
-        "last_name": "Penddreth",
-        "email": "jpenddreth0@census.gov",
-        "gender": "Female",
-        "ip_address": "26.58.193.2"
-    }, {
-        "id": 2,
-        "first_name": "Giavani",
-        "last_name": "Frediani",
-        "email": "gfrediani1@senate.gov",
-        "gender": "Male",
-        "ip_address": "229.179.4.212"
-    }, {
-        "id": 3,
-        "first_name": "Noell",
-        "last_name": "Bea",
-        "email": "nbea2@imageshack.us",
-        "gender": "Female",
-        "ip_address": "180.66.162.255"
-    }, {
-        "id": 4,
-        "first_name": "Willard",
-        "last_name": "Valek",
-        "email": "wvalek3@vk.com",
-        "gender": "Male",
-        "ip_address": "67.76.188.26"
-    }];
+    const informations = await loadProductsCollection();
+    console.log(informations);
+    // informations = [{
+    //     "id": 40,
+    //     "first_name": "Jeanette",
+    //     "last_name": "Penddreth",
+    //     "email": "jpenddreth0@census.gov",
+    //     "gender": "Female",
+    //     "ip_address": "26.58.193.2"
+    // }, {
+    //     "id": 2,
+    //     "first_name": "Giavani",
+    //     "last_name": "Frediani",
+    //     "email": "gfrediani1@senate.gov",
+    //     "gender": "Male",
+    //     "ip_address": "229.179.4.212"
+    // }, {
+    //     "id": 3,
+    //     "first_name": "Noell",
+    //     "last_name": "Bea",
+    //     "email": "nbea2@imageshack.us",
+    //     "gender": "Female",
+    //     "ip_address": "180.66.162.255"
+    // }, {
+    //     "id": 4,
+    //     "first_name": "Willard",
+    //     "last_name": "Valek",
+    //     "email": "wvalek3@vk.com",
+    //     "gender": "Male",
+    //     "ip_address": "67.76.188.26"
+    // }];
     // const informations = await loadProductsCollection();
     res.send(await informations);
 });
@@ -52,6 +53,7 @@ async function loadProductsCollection() {
         });
     });
     return renvoi;
+
 }
 
 module.exports = router;
