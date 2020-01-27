09440cdf54a00ac121d9ef08ed7937fd520820a4
Aredi <dadaslashfaq@gmail.com>
2020-01-27 12:36:12 +0100

    chart js et tailwind en local + stockage html modif

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -4,8 +4,8 @@
 
 document.getElementById("navbar").innerHTML += '<nav class="items-baseline relative bg-black w-full flex flex-wrap items-center justify-between py-2">\n' +
     '    <div class="xs:justify-end justify-center sm:w-auto w-full flex flex-no-shrink items-stretch h-12">\n' +
-    '        <a href="/" class="flex-no-grow flex-no-shrink relative my-2 mx-4 leading-normal text-white no-underline flex items-center hover:bg-grey-dark text-3xl">Interfiltre</a>\n' +
-    '<a href="" class="flex-no-grow flex-no-shrink relative my-2 mx-4 leading-normal text-white no-underline flex items-center hover:bg-grey-dark text-3xl" id="dateheure"></a>\n' +
+    '           <img src="https://zupimages.net/up/20/05/cevd.png" class="flex-no-grow flex-no-shrink relative my-2 mx-4 leading-normal text-white no-underline flex items-center hover:bg-grey-dark text-3xl">' +
+    '<a href="" class="flex-no-grow relative my-2 mx-4 leading-normal text-white no-underline flex items-center hover:bg-grey-dark text-3xl" id="dateheure"></a>\n' +
     '    </div>\n' +
     '    <div class="text-center text-white sm:w-auto w-full  rounded-lg flex-wrap">\n' +
     '        <div class="lg:flex lg:items-stretch lg:justify-end ml-auto">\n' +
