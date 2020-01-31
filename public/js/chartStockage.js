8065af9776d95b57ffa895610855e44ba6443880
Aredi <dadaslashfaq@gmail.com>
2020-01-31 11:37:30 +0100

    animations, chart display...

--- public/js/chartStockage.js
+++ public/js/chartStockage.js
@@ -3,19 +3,26 @@ let newdata = [];
 var graphique = [];
 let labels = [];
 let donnee = [];
+var ctx = document.getElementById('myChart');
+var gradctx =  document.getElementById('myChart').getContext("2d");
+
+
 
 document.addEventListener('DOMContentLoaded', function() {
 
+    var testArray= [{"production":"12","mois":"janvier"},{"production":"5","mois":"février"},{"production":"9","mois":"mars"},{"production":"17","mois":"avril"},{"production":"6","mois":"mai"},{"production":"8","mois":"juin"},{"production":"17","mois":"juillet"},{"production":"7","mois":"aout"},{"production":"10","mois":"septembre"}];
+    var testArray2= [{"production":"5","mois":"Yolomode"},{"production":"5","mois":"février"},{"production":"9","mois":"mars"},{"production":"17","mois":"avril"},{"production":"6","mois":"mai"},{"production":"8","mois":"juin"},{"production":"17","mois":"juillet"},{"production":"7","mois":"aout"},{"production":"10","mois":"septembre"}];
 
-    var ctx = document.getElementById('myChart');
+
+
     async function performGetRequest() {
         let response = await fetch(`http://localhost:4000/api/fulldata`);
         let data = await response.json();
-         donnee = data.map(function (item) {
-            return item.total;
+         donnee = testArray.map(function (item) {
+            return item.production;
         });
-         labels = data.map(function (item) {
-            return item.filtre;
+         labels = testArray.map(function (item) {
+            return item.mois;
         });
 
         newdata = data.map(function(item){
@@ -24,13 +31,25 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log(newdata);
         console.log(donnee);
         console.log(labels);
+
+        //couleurs du graph
+        let gradient = gradctx.createLinearGradient(0, 0, 0, 600);
+
+        //gradient bar & line
+        gradient.addColorStop(0, 'orange');
+        gradient.addColorStop(1, 'purple');
+
+
+
         graphique = new Chart(ctx.getContext('2d'), {
             type: 'bar',
             data: {
                 labels: labels,
                 datasets: [{
                     label: 'Test',
-                    data: donnee
+                    data: donnee,
+                    backgroundColor: gradient,
+                    hoverBackgroundColor:'rgb(64,252,52)'
                 }],
             },
         });
@@ -56,11 +75,21 @@ function sendMail() {
 
 }
 
-function addData(chart, data) {
-    chart.data.datasets.labels = 'Testi';
-    chart.data.datasets.push(data);
-    chart.update();
-    console.log('done');
+function bobDylan() {
+graphique.destroy();
+console.log('destruction du graphique');
+    graphique = new Chart(ctx.getContext('2d'), {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Test',
+                data: newdata
+
+            }],
+        },
+    });
+
 }
 
 
@@ -71,5 +100,63 @@ function removeData(chart) {
     });
     chart.update();
 }
+
+function changeGraphType(type){
+    let gradient = gradctx.createLinearGradient(0, 0, 0, 600);
+
+    //gradient bar & line
+    gradient.addColorStop(0, 'orange');
+    gradient.addColorStop(1, 'purple');
+
+
+    graphique.destroy();
+
+    if(type==='pie'){
+        graphique = new Chart(ctx.getContext('2d'), {
+            type: type,
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Ceci est un test',
+                    data: donnee,
+
+                    backgroundColor: gradient,  //['#FF3254','#85ff2d','#ffdb0f','#FF3254','#FF3254','#FF3254','#FF3254','#FF3254','#FF3254'],
+                    hoverBackgroundColor:'rgb(252,10,19)'
+                }],
+            },
+        });
+    }
+    else if(type==='line'){
+    graphique = new Chart(ctx.getContext('2d'), {
+        type: type,
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Grillage blindax',
+                data: donnee,
+
+                backgroundColor:gradient,
+            }],
+        },
+    });
+    }
+    else{
+        graphique = new Chart(ctx.getContext('2d'), {
+            type: type,
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: 'Test',
+                    data: donnee,
+
+                    backgroundColor:gradient,
+                    hoverBackgroundColor:'rgb(64,252,52)'
+                }],
+            },
+        });
+    }
+
+}
+
 
 
