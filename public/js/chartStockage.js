2b894dc79d146f73131bcc9d1209c75dcce3c754
Aredi <dadaslashfaq@gmail.com>
2020-02-12 14:51:39 +0100

    Update UI

--- public/js/chartStockage.js
+++ public/js/chartStockage.js
@@ -95,7 +95,7 @@ function changeToBar() {
              type: 'bar',
              data: chartData
          });
-     };
+     }
 
 function changeToLine(){
 
@@ -125,5 +125,29 @@ function sendMail() {
 
 }
 
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var apropos = document.getElementById("btnpropos");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+apropos.onclick = function() {
+    modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+};
 
 
